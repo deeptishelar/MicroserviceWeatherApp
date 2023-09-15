a7f80564428dd68d6b53572bca90c430155f69b5
Deepti <deepti.shelar@gmail.com>
2023-09-15 12:54:28 +1000

    refactoring

--- weather/src/main/java/deepti/com/repository/DataRepository.java
+++ weather/src/main/java/deepti/com/repository/DataRepository.java
@@ -3,7 +3,6 @@ package deepti.com.repository;
 import deepti.com.model.Data;
 import deepti.com.model.DataRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
--- weather/src/main/java/deepti/com/service/WeatherService.java
+++ weather/src/main/java/deepti/com/service/WeatherService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Service
@@ -19,56 +18,68 @@ public class WeatherService {
     @Autowired
     DataRepository dataRepository;
 
-
-    public List<Station> getWeatherStations1() {
-        List<WeatherStation> all = weatherRepository.findAll();
-        List<Station> stations = new ArrayList<>();
-        all.forEach(weatherStation ->
-        {
-            stations.add(new Station(weatherStation.getId(), weatherStation.getLatitude(), weatherStation.getLongitude(), weatherStation.getWsName()));
-        });
-        return stations;
-    }
     public List<WeatherStation> getWeatherStations() {
         return weatherRepository.findAll();
     }
 
     public WeatherInfo getWeatherInfo(String wsId) {
-        WeatherStation weatherStation = weatherRepository.findById(Integer.parseInt(wsId)).get();
-        List<Variable> variables = weatherStation.getVariable();
-        StringBuilder sql = new StringBuilder("SELECT ");
-        sql.append(" timestamp, ");
-        for (Variable var : variables) {
-            String name = var.getName();
-            sql.append(name);
-            sql.append(",");
+        WeatherStation weatherStation = weatherRepository.findById(Integer.parseInt(wsId)).isPresent() ?
+                weatherRepository.findById(Integer.parseInt(wsId)).get() : null;
+        if(weatherStation == null)
+        {
+            throw new RuntimeException("Weather Station with given id is not present : "+wsId);
         }
-        sql.replace(sql.lastIndexOf(","), sql.length(),"");
-        sql.append(" FROM data_").append(weatherStation.getId());
-        sql.append(" ORDER BY timestamp desc limit 1");
-
-        System.out.println(sql.toString());
+        List<Variable> variables = weatherStation.getVariable();
+        StringBuilder sql = buildDataSql(variables, String.valueOf(weatherStation.getId()));
         List<VariableValueUnit> dataList = new ArrayList<>();
-        Data data = dataRepository.getData(sql.toString()).isPresent()? dataRepository.getData(sql.toString()).get() : null;
-        String time = "";
-        if(data != null) {
-            List<Double> values = data.getValues();
-            int index = 0;
-            if (variables.size() == values.size()) {
-                for (Variable var : variables) {
-                    dataList.add(new VariableValueUnit(var.getLongName(), var.getUnit(), values.get(index++)));
+        String time;
+        try {
+            Data data = dataRepository.getData(sql.toString()).isPresent() ? dataRepository.getData(sql.toString()).get() : null;
+
+            if (data == null) {
+                throw new RuntimeException("Data for given weather station id is not present : " + wsId);
+            } else {
+                List<Double> values = data.getValues();
+                int index = 0;
+                if (variables.size() == values.size()) {
+                    for (Variable var : variables) {
+                        dataList.add(new VariableValueUnit(var.getLongName(), var.getUnit(), values.get(index++)));
+                    }
                 }
+                time = formatDate(data.getTimestamp());
             }
-            time = formatDate(data.getTimestamp());
+        }catch(Exception e)
+        {
+            throw new RuntimeException("Error occurred while fetching weather data, please make sure the data is populated in the DB");
         }
         WeatherInfo weatherInfo = new WeatherInfo(weatherStation.getWsName(),
                 weatherStation.getPortfolio(), weatherStation.getSite(),
                 dataList, time);
-        System.out.println(weatherInfo.toString());
+        System.out.println(weatherInfo);
         return weatherInfo;
     }
-    private String formatDate(Date date)
-    {
+
+    private static StringBuilder buildDataSql(List<Variable> variables, String wsId) {
+        if(!variables.isEmpty()) {
+            StringBuilder sql = new StringBuilder("SELECT ");
+            sql.append(" timestamp, ");
+            for (Variable var : variables) {
+                String name = var.getName();
+                sql.append(name);
+                sql.append(",");
+            }
+            sql.replace(sql.lastIndexOf(","), sql.length(), "");
+            sql.append(" FROM data_").append(wsId);
+            sql.append(" ORDER BY timestamp desc limit 1");
+            return sql;
+        }
+        else {
+            throw new RuntimeException("No variable found for given weather station  : "+wsId);
+        }
+
+    }
+
+    private String formatDate(Date date) {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
         return dateFormat.format(date);
     }
